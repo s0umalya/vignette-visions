9b715aa48c825fda13309c0053b501e1b1a3f9ad
Soumalya Chakraborty <soumalya.chk@gmail.com>
2026-06-04 15:41:03 +0530

    Create reusable photo card component

--- frontend/web-app-angular/src/app/shared/components/photo-card/photo-card.component.ts
+++ frontend/web-app-angular/src/app/shared/components/photo-card/photo-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { Photo } from '../../models/photo.model';
 
 @Component({
   selector: 'app-photo-card',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './photo-card.component.html',
   styleUrl: './photo-card.component.scss'
 })
