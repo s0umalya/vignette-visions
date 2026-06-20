ac1549cc01f8350613a9617c027379fa4adf8e5d
Soumalya Chakraborty <soumalya.chk@gmail.com>
2026-06-21 02:10:18 +0530

    Dynamic navbar greeting

--- frontend/web-app-angular/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/web-app-angular/src/app/shared/components/navbar/navbar.component.ts
@@ -1,17 +1,29 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgClass, RouterLink],
+  imports: [NgClass, RouterLink, MatMenuModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   isMenuOpen = false;
 
+  protected authService = inject(AuthService);
+  private router = inject(Router);
+
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/']);
   }
 }
